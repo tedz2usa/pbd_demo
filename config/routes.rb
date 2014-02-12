8ae6a61dba090455f9366843b08d6098e4464dcd
Ted Zhu <tedz2usa@gmail.com>
2014-02-12 03:51:27 -0500

    Create new 'main' controller, with primative view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 PbdDemo::Application.routes.draw do
+  get "main/index"
+
+  root "main#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
