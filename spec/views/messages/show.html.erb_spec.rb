c5cdb5720b4d1234dfc17f3bdc6debb273a9e988
Ted Zhu <tedz2usa@gmail.com>
2014-03-04 10:03:12 -0500

    Remove irrelevant files.

--- spec/views/messages/show.html.erb_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'messages/show.html.erb' do
-  it 'displays the text attribute of the message' do
-    assign(:message, double('Message', :text => 'Hello World!'))
-    render
-    rendered.should contain('Hello World')
-  end
-end
